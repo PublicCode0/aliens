0d73fc6ddff0fc8c8112f3196bcee398743be76f
PublicCode0 <141111415+PublicCode0@users.noreply.github.com>
2023-08-01 10:58:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+// function effect()
+// {
+//     let x= document.getElementById("para1");
+//     // alert(x.innerHTML);
+//     x.style.color="red";
+// }
+// function uneffect()
+// {
+//     let x = document.getElementById("para1");
+//     x.style.color="black"
+// }
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// function showresult()
+// {
+//     let x=document.getElementsByName("yes");
+//     x.style.backgroundColor="red";
+// }
+// function clearw(){
+//     let x=document.getElementsByTagName("form");
+//     x.style.backgroundColor="white";
+// }
+// --------------------------------------------------------------------------------------------------------------------------------------------
+
+// function validation()
+// { 
+//     var result = true;
+//     var i = document.getElementsByTagName("input");
+//     if(i[0].value.length==0)
+//     result=false;
+    
+//     return(result);
+    
+// }
+// --------------------------------------------------------------------------------------------------------------------------------------
+
+// function  validation()
+// {
+//    var result = true;
+//    var e = document.getElementsByName("email") [0].value;
+
+
+//    var atindex = e.indexOf('@');
+//    var dotindex = e.lastIndexOf('.');
+//    if(atindex<1 || dotindex>=e.length-2 || dotindex-atindex<3)
+//    result = false;
+
+//    return(result);
+
+// }
+
+function confbox(){
+alert("You want to see the alien");
+}
